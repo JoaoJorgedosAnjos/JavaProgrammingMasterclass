f08e60f3c22652e51e7ddc0a3029b3d20c539aec
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-18 14:35:42 -0300

    refatorando nome do projeto

---
+++ src/section04/helloWorld/Main.java
@@ -0,0 +1,10 @@
+package section04.helloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+        int my = Integer.MAX_VALUE;
+        System.out.println(my);
+        System.out.println('a');
+        System.out.println('a');
+    }
+}
