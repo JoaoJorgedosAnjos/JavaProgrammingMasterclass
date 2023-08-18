d256ca123035bf73d32fe8227f9c1c27da0d6c44
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-18 16:17:40 -0300

    34 - if then statement

---
+++ src/section04/ifThenStatement/Main.java
@@ -0,0 +1,12 @@
+package section04.ifThenStatement;
+
+public class Main {
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+    }
+}
