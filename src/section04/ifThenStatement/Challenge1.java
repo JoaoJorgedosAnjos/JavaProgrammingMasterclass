bf3822b31db80c041f6ee5d6d46330dae8071888
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-19 18:04:54 -0300

    Section 4 completed

---
+++ src/section04/ifThenStatement/Challenge1.java
@@ -0,0 +1,25 @@
+package section04.ifThenStatement;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        double myFirstValue = 20.0d, mySecondValue = 80, myValuesTotal, theRemainder;
+        boolean isNoRemainder = true;
+
+        myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+
+        theRemainder = myFirstValue % 40.00d;
+
+        isNoRemainder = (theRemainder == 0);
+        // or
+        //isNoRemainder = (theRemainder == 0) ? true : false;
+
+        System.out.println(theRemainder);
+        System.out.println(isNoRemainder);
+
+        if (!isNoRemainder){
+            System.out.println("Got some remainder");
+        }
+
+
+    }
+}
--- src/section04/ifThenStatement/Main.java
+++ src/section04/ifThenStatement/Main.java
@@ -2,11 +2,51 @@ package section04.ifThenStatement;
 
 public class Main {
     public static void main(String[] args) {
-
+        //If then Statement
         boolean isAlien = false;
         if (isAlien == false) {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
-    }
+        //Locigal and Operator
+        int topScore = 80;
+        if(topScore < 100){
+            System.out.println("You got high score!");
+        }
+        //Locial Or
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <=90)){
+            System.out.println("Either or both of the condition are true");
+        }
+        //Assignment Operator vs equals to Operator
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is true");
+        }
+        boolean isCar = false;
+        if(isCar = true){
+            System.out.println("This is not supposed to happen");
+        }
+        if(isCar == false){ // == or !=
+            System.out.println("This is not supposed to happen");
+        }
+        //Ternary Operator
+        // operand1 ? operand2 : operand3 if this ? do this :if not do this
+
+        String makeOfCAr = "Volkswagen";
+        boolean isDomestic = makeOfCAr == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to out country");
+        }
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+
+
+        }
 }
