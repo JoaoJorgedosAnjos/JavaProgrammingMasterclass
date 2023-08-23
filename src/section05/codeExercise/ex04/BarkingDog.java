f1e64f80ab7f7ef688975fd91e28bb6b489ab909
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-23 17:39:36 -0300

    Section 5 code exercise 04

---
+++ src/section05/codeExercise/ex04/BarkingDog.java
@@ -0,0 +1,17 @@
+package section05.codeExercise.ex04;
+
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println( shouldWakeUp(true,4));
+    }
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        }
+
+        if (barking && (hourOfDay < 8 || hourOfDay > 22)) {
+            return true;
+        }
+        return false;
+    }
+}
