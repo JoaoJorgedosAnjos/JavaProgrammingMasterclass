2740a60f6dc556b2acef71fc430cbef6bf322718
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-22 23:17:38 -0300

    Section 5 code exercise 03

---
+++ src/section05/codeExercise/ex03/MegaBytesConverter.java
@@ -0,0 +1,19 @@
+package section05.codeExercise.ex03;
+
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(0);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        int megabytes = kiloBytes / 1024;
+        int remainingKilobytes = kiloBytes % 1024;
+
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKilobytes + " KB");
+
+        }
+    }
+}
