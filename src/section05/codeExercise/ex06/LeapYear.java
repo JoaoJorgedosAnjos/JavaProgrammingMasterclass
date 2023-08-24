0d4ced82484ada3c367c79bf42ef02c977eae039
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-24 01:03:23 -0300

    Section 5 code exercise 05

---
+++ src/section05/codeExercise/ex06/LeapYear.java
@@ -0,0 +1,20 @@
+package section05.codeExercise.ex06;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1924));
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 != 0 || year % 400 == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+}
