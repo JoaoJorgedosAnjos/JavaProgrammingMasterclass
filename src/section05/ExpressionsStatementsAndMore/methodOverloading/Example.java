0e4333c1844bd20f574e5c20181bcfd34662449d
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-24 15:39:53 -0300

    Method Overloading

---
+++ src/section05/ExpressionsStatementsAndMore/methodOverloading/Example.java
@@ -0,0 +1,19 @@
+package section05.ExpressionsStatementsAndMore.methodOverloading;
+
+public class Example {
+    public static void doSomething(int parameterA){
+
+    }
+    public static void doSomething(float parameterA){
+
+    }
+    public static void doSomething(int parameterA, float parameterB){
+            //same paramter but in diffenent order
+    }
+    public static void doSomething(float parameterA, int parameterB){
+
+    }
+    public static void doSomething(int parameterA, int parameterB, float parameterC){
+
+    }
+}
