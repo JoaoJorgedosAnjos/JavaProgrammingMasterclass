88ca0e57431dbf59c4ffe764bbf50991aa685851
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-24 14:04:58 -0300

    Section 5 code exercise 08

---
+++ src/section05/codeExercise/ex08/TeenNumberChecker.java
@@ -0,0 +1,19 @@
+package section05.codeExercise.ex08;
+
+public class TeenNumberChecker {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(10,20,10));
+    }
+    public static boolean hasTeen(int ageCheck1, int ageCheck2, int ageCheck3) {
+        if ((ageCheck1 >= 13 && ageCheck1 <= 19) || (ageCheck2 >= 13 && ageCheck2 <= 19) || (ageCheck3 >= 13 && ageCheck3 <= 19)) {
+            return true;
+        }
+        return false;
+    }
+    public static boolean isTeen(int ageCheck){
+        if (ageCheck >= 13 && ageCheck <=19)
+            return true;
+        return false;
+    }
+}
+
