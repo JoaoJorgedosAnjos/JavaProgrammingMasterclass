f474d4e594b23d7957e64b9cbe6bc58e150f3489
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-26 01:20:49 -0300

    Section 05 Code exercise 09

---
+++ src/section05/ExpressionsStatementsAndMore/codeExercise/ex09/AreaCalculator.java
@@ -0,0 +1,24 @@
+package section05.ExpressionsStatementsAndMore.codeExercise.ex09;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+        System.out.println(area(5));
+        System.out.println(area(-1));
+        System.out.println(area(5.0,4d));
+        System.out.println(area(-1,4d));
+    }
+
+    public static double area(double radius) {
+        if (radius < 0)
+            return -1;
+        else
+            return radius * radius * Math.PI;
+    }
+
+    public static double area(double x, double y){
+        if (x < 0 || y < 0)
+            return -1;
+        else
+            return x * y;
+    }
+}
