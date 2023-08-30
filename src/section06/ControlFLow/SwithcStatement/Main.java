1f3686b5ef0b089298adbad6231ce018a7aeae58
joaojorge <joaojorgedosanjos@gmail.com>
2023-08-29 23:36:18 -0300

    Section 06 switch case example enhanced

--- src/section06/ControlFLow/SwithcStatement/Main.java
+++ src/section06/ControlFLow/SwithcStatement/Main.java
@@ -4,19 +4,35 @@ public class Main {
     public static void main(String[] args) {
 
         int switchValue = 3;
+        //Old Switch
         switch (switchValue) {
             case 1:
                 System.out.println("Value was 1");
                 break;
             case 2:
                 System.out.println("Value was 2");
-            case 3 : case 4 : case  5:
+            case 3:
+            case 4:
+            case 5:
                 System.out.println("Value was 3, a 4, or 5");
                 System.out.println("Actually it was a " + switchValue);
                 break;
             default:
                 System.out.println("Was  not 1, 2, 3, 4 or 5");
                 break;
+        }
+
+        //Switch enhanced
+        switch (switchValue) {
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
+
+
+                System.out.println("Value was 3, a 4, or 5");
+                System.out.println("Actually it was a " + switchValue);
+            }
+            default -> System.out.println("Was  not 1, 2, 3, 4 or 5");
         }
     }
 }
