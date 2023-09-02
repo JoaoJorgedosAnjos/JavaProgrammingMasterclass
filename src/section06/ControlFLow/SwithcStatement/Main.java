a5536e3e73739a89fdbe73133e1c37519e54c9fd
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-01 23:54:18 -0300

    Section 06 switch case example enhanced withc methods

--- src/section06/ControlFLow/SwithcStatement/Main.java
+++ src/section06/ControlFLow/SwithcStatement/Main.java
@@ -34,5 +34,39 @@ public class Main {
             }
             default -> System.out.println("Was  not 1, 2, 3, 4 or 5");
         }
+
+        String month = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter ");
     }
+
+    public static String getQuarter(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVERBER", "DECEMBER" -> "4th";
+            default -> "bad";
+        };
+    }
+    /*public static String getQuarter(String month) {
+        switch (month) {
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVERBER":
+            case "DECEMBER":
+                return "4th";
+        }
+        return "bad";
+    }*/
 }
