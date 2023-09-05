68f188ed64c547e53530e502d13bca0df13b3c61
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-05 11:58:39 -0300

    Section 06 digitSumChallenge

---
+++ src/section06/ControlFLow/looping/whileExample/digitSumChallenge/Main.java
@@ -0,0 +1,19 @@
+package section06.ControlFLow.looping.whileExample.digitSumChallenge;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigits(125));
+    }
+    static int sumDigits(int number){
+
+        if (number < 0) return -1;
+
+        int sum = 0;
+         while ( number > 9){
+             sum += (number % 10);
+             number = number / 10;
+         }
+         sum += number;
+        return sum;
+    }
+}
