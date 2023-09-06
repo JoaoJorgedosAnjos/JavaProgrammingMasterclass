19301be01d948f3731a244c7caeaaca4aaff0803
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-06 15:51:54 -0300

    Section 06 code exercise 20

--- src/section06/ControlFLow/codeExercise/ex20/LastDigitChecker.java
+++ src/section06/ControlFLow/codeExercise/ex20/LastDigitChecker.java
@@ -2,13 +2,35 @@ package section06.ControlFLow.codeExercise.ex20;
 
 public class LastDigitChecker {
     public static void main(String[] args) {
-        System.out.println(hasSameLastDigit(1000,1000,1001));
+        System.out.println(hasSameLastDigit(92, 90, 92));
     }
 
     public static boolean hasSameLastDigit(int number1, int number2, int number3) {
-        if ((number1 < 10 || number1 > 1000) || (number2 < 10 || number2 > 1000) || (number3 < 10 || number3 > 1000))
-            return false;
+        if (!isValid(number1) || (!isValid(number2)) || (!isValid(number3))) return false;
+        int rightMostDigit1 = number1 % 10;
+        int rightMostDigit2 = number2 % 10;
+        int rightMostDigit3 = number3 % 10;
 
-        return true;
+        return ((rightMostDigit1 == rightMostDigit2) || (rightMostDigit1 == rightMostDigit3) || (rightMostDigit2 == rightMostDigit3));
+    }
+
+//        public static boolean hasSameLastDigit(int number1, int number2, int number3) {
+//        if (!isValid(number1) || !isValid(number2) || !isValid(number3)) {
+//            return false;
+//        }
+//
+//        int rightMostDigit1 = number1 % 10;
+//        int rightMostDigit2 = number2 % 10;
+//        int rightMostDigit3 = number3 % 10;
+//
+//        if (rightMostDigit1 == rightMostDigit2 || rightMostDigit1 == rightMostDigit3 || rightMostDigit2 == rightMostDigit3) {
+//            return true;
+//        }
+//
+//        return false;
+ //   }
+
+    public static boolean isValid(int number) {
+        return number >= 10 && number <= 1000;
     }
 }
