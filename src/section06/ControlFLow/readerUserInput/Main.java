c1f81b8a895a2462e2738421c0dc77d1abaa60a9
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-07 22:27:42 -0300

    Section 06 reader user input System.console

---
+++ src/section06/ControlFLow/readerUserInput/Main.java
@@ -0,0 +1,27 @@
+package section06.ControlFLow.readerUserInput;
+
+public class Main {
+    public static void main(String[] args) {
+        int currentYear = 2023;
+
+        System.out.println(getInputFromConsole(currentYear));
+
+        System.out.println(getInputFromScanner(currentYear));
+
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, What's your Name?");
+        System.out.println("Hi " + name + ", Thanks for talking the course");
+
+        String dateOfBirth = System.console().readLine("What year were your born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " year old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+        return "";
+    }
+}
