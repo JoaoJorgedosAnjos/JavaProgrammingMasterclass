d7bdcfeebce4a8f909ba614cd9f6e91ac9f6ee7a
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-11 18:12:36 -0300

    Section 06 reader input and reader input challenge

--- src/section06/ControlFLow/readerUserInput/Main.java
+++ src/section06/ControlFLow/readerUserInput/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         try {
             System.out.println(getInputFromConsole(currentYear));
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println(getInputFromScanner(currentYear));
         }
 
@@ -27,9 +27,9 @@ public class Main {
 
     public static String getInputFromScanner(int currentYear) {
 
-       Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-       // String name = System.console().readLine("Hi, What's your Name?");
+        // String name = System.console().readLine("Hi, What's your Name?");
         System.out.println(("Hi, What's your Name?"));
         String name = scanner.nextLine();
 
@@ -37,9 +37,31 @@ public class Main {
 
         //String dateOfBirth = System.console().readLine("What year were your born?");
         System.out.println("What year were you born?");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDOB = age < 0 ? false : true;
+            }catch (NumberFormatException badUserData){
+                System.out.println("Character not allowed!!! Try again.");
+            }
+        } while (!validDOB);
 
         return "So you are " + age + " year old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return (currentYear - dob);
     }
 }
