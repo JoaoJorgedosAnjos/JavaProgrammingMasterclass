60888b28ce2c72312b5e336aff89e121e46a95e3
joaojorge <joaojorgedosanjos@gmail.com>
2023-09-11 18:12:52 -0300

    Section 06 min and max challenge

---
+++ src/section06/ControlFLow/minAndMaxChallenge/Main.java
@@ -0,0 +1,36 @@
+package section06.ControlFLow.minAndMaxChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double max = 0, min = 0;
+        int loopCount = 0;
+
+        while (true) {
+            System.out.println("Enter a number, or any character to exit: ");
+            String nextEntry = scanner.nextLine();
+            try {
+                double validNumber = Double.parseDouble(nextEntry);
+                if (loopCount == 0 || validNumber < min) {
+                    min = validNumber;
+                }
+                if (loopCount == 0 || validNumber > max) {
+                    max = validNumber;
+                }
+                loopCount++;
+            } catch (NumberFormatException e) {
+                break;
+            }
+        }
+        if (loopCount > 0){
+            System.out.println( "min = " + min + ", max = " + max);
+        }else {
+            System.out.println("No valid data entered");
+        }
+
+    }
+}
